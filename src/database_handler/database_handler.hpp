c11842ae3c3a59ac5d57a1595aab222b63e49bb2
Josh Baker <JoshBaker529@Gmail.com>
2025-08-12 12:27:25 -0400

    Removed sqlite3pp from project files.

--- src/database_handler/database_handler.hpp
+++ src/database_handler/database_handler.hpp
@@ -4,8 +4,8 @@
 #ifndef DATABASE_HANDLER_H
 #define DATABASE_HANDLER_H
 
-#include "sqlite3pp-master/headeronly_src/sqlite3pp.h"
 #include <ctime>
+#include <sqlite3pp-master/headeronly_src/sqlite3ppext.h>
 
 template <class T> class DatabaseHandler {
 public:
@@ -16,5 +16,14 @@ public:
 private:
   sqlite3pp::database db;
 };
+
+// TODO:
+// Main thought for updating cache:
+// Use websockets to communicate updates.
+// When a change is made, send a request to every other process
+//    to update their cache.
+// When recieving a request, re-process the query
+// .
+// WHICH APPARENTLY C++ DOESN'T HAVE WEBSOCKETS BUILT INTO IT!
 
 #endif // !DATABASE_HANDLER_H
