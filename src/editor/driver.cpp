ea57bf21d0b9cda46309b4cd148ec30599e8b9fe
Josh Baker <JoshBaker529@Gmail.com>
2025-08-15 10:36:31 -0400

    Implemented barebones editor for adding events

---
+++ src/editor/driver.cpp
@@ -0,0 +1,36 @@
+
+#include "../database_handler/database_handler.hpp"
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+
+int main() {
+  DatabaseHandler db;
+  std::string action, date, class_id, desc, sched, important;
+
+  // Full disclosure, I will not keep this 100% like this.
+  // I will add a way to add bulk items
+  // This is just simple code to test the calendar
+  while (true) {
+    cout << "Add/Modify/Remove: ";
+    std::getline(cin, action);
+
+    if (action == "add") {
+      cout << "\nDate in YYYY-MM-DD format: ";
+      std::getline(cin, date);
+      cout << "\nEnter class ID: ";
+      std::getline(cin, class_id);
+      cout << "\nEnter Description: ";
+      std::getline(cin, desc);
+      cout << "\nIs this a schedule item? [TRUE/FALSE]: ";
+      cin >> sched;
+      cout << "\nIs this an important item? [TRUE/FALSE]: ";
+      cin >> important;
+
+      db.add_event(date, class_id, desc, sched, important);
+      db.debug_print_db();
+    }
+  }
+  return 0;
+}
