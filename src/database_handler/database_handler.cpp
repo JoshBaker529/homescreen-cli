a3862535951465a792c6fe60b701c9d44aa97312
Josh Baker <JoshBaker529@Gmail.com>
2025-08-15 13:10:23 -0400

    added sub_days_to_date(). Moved add_days_to_date() to outside class. Small modifications to make more useful

--- src/database_handler/database_handler.cpp
+++ src/database_handler/database_handler.cpp
@@ -2,9 +2,12 @@
 #include "database_handler.hpp"
 #include "sqlite3pp-master/headeronly_src/sqlite3pp.h"
 #include <ctime>
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 #include <iostream>
+#include <vector>
 
 // These are -1 to account for the first day.
 // ie Sunday is 1, Saturday is 6 days ahead of that
@@ -56,7 +59,7 @@ void DatabaseHandler::add_event(string date, string class_id, string desc,
   cmd.execute();
 }
 
-std::string DatabaseHandler::get_events_month_string(struct tm *date) {
+EVENT_MAP DatabaseHandler::get_events_month(struct tm *date) {
   struct tm start = get_start_of_week(date);
   struct tm end = add_days_to_date(&start, DAYS_MONTH);
 
@@ -64,12 +67,42 @@ std::string DatabaseHandler::get_events_month_string(struct tm *date) {
                         date_to_string(&start) + "' AND '" +
                         date_to_string(&end) + "'";
 
-  return request;
+  sqlite3pp::query qry(db, request.c_str());
+
+  std::vector<event> vec;
+
+  for (sqlite3pp::query::iterator i = qry.begin(); i != qry.end(); ++i) {
+    struct event item((*i).get<string>(0), (*i).get<string>(1),
+                      (*i).get<string>(2), string_to_bool((*i).get<string>(3)),
+                      string_to_bool((*i).get<string>(4)),
+                      string_to_bool((*i).get<string>(5)));
+    vec.push_back(item);
+    /*
+    for (int j = 0; j < qry.column_count(); ++j) {
+      std::cout << (*i).get<char const *>(j) << "\t";
+    }
+
+    std::cout << std::endl;
+    */
+  }
+
+  EVENT_MAP map = organize_events(vec);
+
+  return map;
 }
 
 std::string DatabaseHandler::date_to_string(struct tm *date) {
-  return std::to_string(date->tm_year) + "-" + std::to_string(date->tm_mon) +
-         "-" + std::to_string(date->tm_mday);
+
+  std::stringstream ss;
+  ss << std::setfill('0');
+  ss << date->tm_year << "-" << std::setw(2) << date->tm_mon << std::setw(1)
+     << "-" << std::setw(2) << date->tm_mday;
+
+  return ss.str();
+  /*
+    return std::to_string(date->tm_year) + "-" + std::to_string(date->tm_mon) +
+           "-" + std::to_string(date->tm_mday);
+           */
 }
 
 struct tm DatabaseHandler::get_start_of_week(struct tm *date) {
@@ -78,7 +111,7 @@ struct tm DatabaseHandler::get_start_of_week(struct tm *date) {
   return updated;
 }
 
-struct tm DatabaseHandler::add_days_to_date(struct tm *date, int days) {
+struct tm add_days_to_date(struct tm *date, int days) {
   struct tm temp = *date;
   temp.tm_mday = (temp.tm_mday + days);
   if (temp.tm_mday > days_in_month[temp.tm_mon]) {
@@ -87,4 +120,38 @@ struct tm DatabaseHandler::add_days_to_date(struct tm *date, int days) {
   }
 
   return temp;
+}
+
+struct tm sub_days_to_date(struct tm *date, int days) {
+  struct tm temp = *date;
+  temp.tm_mday = (temp.tm_mday - days);
+  if (temp.tm_mday < 1) {
+    temp.tm_mon--;
+    temp.tm_mday += days_in_month[temp.tm_mon];
+  }
+
+  return temp;
+}
+
+EVENT_MAP DatabaseHandler::organize_events(std::vector<event> vec) {
+  EVENT_MAP map;
+
+  for (auto i = vec.begin(); i != vec.end(); i++) {
+    map[i->date].push_back(*i);
+    // std::cout << "Added " << (*i) << " to date " << i->date << "\n";
+  }
+
+  return map;
+}
+
+bool DatabaseHandler::string_to_bool(string s) {
+  if (s == "TRUE")
+    return true;
+  return false;
+}
+
+string DatabaseHandler::bool_to_string(bool b) {
+  if (b)
+    return "TRUE";
+  return "FALSE";
 }
--- src/database_handler/database_handler.hpp
+++ src/database_handler/database_handler.hpp
@@ -5,12 +5,52 @@
 #define DATABASE_HANDLER_H
 
 #include <ctime>
+#include <iostream>
+#include <map>
 #include <sqlite3pp-master/headeronly_src/sqlite3pp.h>
 #include <string>
+#include <vector>
 using std::string;
 
 static int days_in_month[] = {0,  31, 28, 31, 30, 31, 30,
                               31, 31, 30, 31, 30, 31};
+
+struct event {
+  string date;
+  string class_id;
+  string description;
+  bool finished;
+  bool sched;
+  bool important;
+
+  bool operator>(const event &rhs) const {
+    if (this->date == rhs.date) {
+      if (this->important && !rhs.important)
+        return true;
+      return false;
+    }
+
+    return this->date.compare(rhs.date) > 0;
+  }
+
+  event(string d, string c, string desc, bool f, bool s, bool i)
+      : date(d), class_id(c), description(desc), finished(f), sched(s),
+        important(i) {}
+
+  friend std::ostream &operator<<(std::ostream &os, const event &e);
+};
+
+inline std::ostream &operator<<(std::ostream &os, const event &e) {
+  os << "{ date: " << e.date << ", class: " << e.class_id
+     << ", desc: " << e.description << ", finished: " << e.finished
+     << ", sched: " << e.sched << ", important: " << e.important << " }";
+  return os;
+}
+
+struct tm add_days_to_date(struct tm *date, int days);
+struct tm sub_days_to_date(struct tm *date, int days);
+
+typedef std::map<string, std::vector<event>> EVENT_MAP;
 
 class DatabaseHandler {
 public:
@@ -19,13 +59,15 @@ public:
   void debug_print_db();
   void add_event(string date, string class_id, string desc, string sched,
                  string important);
-  std::string get_events_month_string(struct tm *start);
+  EVENT_MAP get_events_month(struct tm *start);
 
 private:
   sqlite3pp::database db;
   std::string date_to_string(struct tm *date);
   struct tm get_start_of_week(struct tm *date);
-  struct tm add_days_to_date(struct tm *date, int days);
+  EVENT_MAP organize_events(std::vector<event> list);
+  string bool_to_string(bool b);
+  bool string_to_bool(string s);
 };
 
 // TODO:
