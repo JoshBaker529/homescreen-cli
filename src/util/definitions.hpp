e01bbee559d8e8ce00eb229a51a83cae521ca232
Josh Baker <JoshBaker529@Gmail.com>
2025-08-19 11:48:10 -0400

    Added sched struct

--- src/util/definitions.hpp
+++ src/util/definitions.hpp
@@ -12,4 +12,21 @@ typedef std::map<string, std::vector<event>> EVENT_MAP;
 static int days_in_month[] = {0,  31, 28, 31, 30, 31, 30,
                               31, 31, 30, 31, 30, 31};
 
+static std::map<string, string> foreground_color{
+    {"default", "39"},      {"black", "30"},      {"red", "31"},
+    {"green", "32"},        {"yellow", "33"},     {"blue", "34"},
+    {"magenta", "35"},      {"cyan", "36"},       {"light gray", "37"},
+    {"dark gray", "90"},    {"light red", "91"},  {"light green", "92"},
+    {"light yellow", "93"}, {"light blue", "94"}, {"light magenta", "95"},
+    {"light cyan", "96"},   {"white", "97"}};
+
+static std::map<string, string> background_color{
+    {"default", "49"},       {"black", "40"},       {"red", "41"},
+    {"green", "42"},         {"yellow", "43"},      {"blue", "44"},
+    {"magenta", "45"},       {"cyan", "46"},        {"light gray", "47"},
+    {"dark gray", "100"},    {"light red", "101"},  {"light green", "102"},
+    {"light yellow", "103"}, {"light blue", "104"}, {"light magenta", "105"},
+    {"light cyan", "106"},   {"white", "107"}};
+
+static string color_reset = "\033[39;49m";
 #endif // DEFINITIONS_HPP
---
+++ src/util/sched.hpp
@@ -0,0 +1,46 @@
+
+#include <cstddef>
+#include <iostream>
+#include <string>
+
+struct sched {
+  int day;
+  std::string class_id;
+  std::string location;
+  std::string start;
+  std::string end;
+
+  bool operator<(const sched &rhs) const {
+    int leftHour = std::stoi(start.substr(0, 2));
+    int leftMinute = std::stoi(start.substr(3, 2));
+    int leftAmPm = start.substr(5, 2) == "AM" ? 0 : 1;
+
+    int rightHour = std::stoi(rhs.start.substr(0, 2));
+    int rightMinute = std::stoi(rhs.start.substr(3, 2));
+    int rightAmPm = rhs.start.substr(5, 2) == "AM" ? 0 : 1;
+
+    if (leftAmPm < rightAmPm)
+      return true;
+    if (leftAmPm == rightAmPm) {
+      if (leftHour < rightHour)
+        return true;
+      if (leftHour == rightHour) {
+        if (leftMinute < rightMinute)
+          return true;
+      }
+    }
+
+    return false;
+  }
+
+  sched(int d, std::string c, std::string l, std::string s, std::string e)
+      : day(d), class_id(c), location(l), start(s), end(e) {}
+
+  friend std::ostream &operator<<(std::ostream &os, const sched &e);
+};
+inline std::ostream &operator<<(std::ostream &os, const sched &e) {
+  os << "{ day: " << e.day << ", class: " << e.class_id
+     << ", loc: " << e.location << ", start: " << e.start << ", end: " << e.end
+     << " }";
+  return os;
+}
